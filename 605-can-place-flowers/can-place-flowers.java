7cbb990ca1c4a65749ff54b7604cc97672c16edb
Meenakshi S <meenakshi.s06ind@gmail.com>
2026-09-18 23:03:32 +0530

    Time: 1 ms (98.66%) | Memory: 47.9 MB (19.02%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int N = flowerbed.length;
+        for(int i=0; i<N; i++){
+            if(flowerbed[i] == 0){
+                boolean left = (i == 0 || flowerbed[i-1] == 0);
+                boolean right = (i == N-1 || flowerbed[i+1] == 0);
+                if(left && right){
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+
+        return n <= 0;
+    }
+}
